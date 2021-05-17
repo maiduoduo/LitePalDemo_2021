3353ef8a4c6acdcf91f0cc25f070d33066e8988d
maiduoduo <jfox.zxj@gmail.com>
2021-05-17 10:40:52 +0800

    LitePal数据库操作示例

---
+++ app/src/main/java/come/litepal/sample/MyApplication.java
@@ -0,0 +1,15 @@
+package come.litepal.sample;
+
+import android.app.Application;
+
+import org.litepal.LitePal;
+
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LitePal.initialize(this);
+    }
+}
---
+++ app/src/main/java/come/litepal/sample/model/User.java
@@ -0,0 +1,43 @@
+package come.litepal.sample.model;
+
+import org.litepal.crud.DataSupport;
+
+
+public class User extends DataSupport {
+    private int id;
+    private String name;
+    private String sex;
+    private String age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
